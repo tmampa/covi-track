e6339e352c867d839687368e563b733ad9ab1831
tmampa <tmampa94@gmail.com>
2022-02-11 08:35:12 +0200

    Add details page

--- src/components/DetailsPage.js
+++ src/components/DetailsPage.js
@@ -4,7 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { IoMdArrowRoundBack } from '@react-icons/all-files/io/IoMdArrowRoundBack';
 import { NavLink } from 'react-router-dom';
 import Header from './Header';
-import Banner from '../asset/Banner.jpeg';
+import Banner from '../assets/Banner.jpeg';
 import { fetchDataApi } from '../redux/covid19Data/covid19Data';
 
 const DetailsPage = () => {
